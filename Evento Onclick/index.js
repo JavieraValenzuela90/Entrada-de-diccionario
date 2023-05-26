5cc12f5416bee801b56cd68df46d3b782d2ffc81
JavieraValenzuela90 <javiera.valenzuela.p@gmail.com>
2023-05-26 09:57:23 -0400

    tarea terminada Evento Onclick

--- Evento Onclick/index.js
+++ Evento Onclick/index.js
@@ -1,4 +1,4 @@
-function buttonstate(elemento) {
+function buttonState(elemento) {
     if (elemento.innerHTML == "Log In") {
         elemento.innerHTML = "Log Out"
     }
@@ -7,13 +7,13 @@ function buttonstate(elemento) {
         }
 }
 
-function addbutton(elemento) {
+function addButton(elemento) {
     elemento.remove();
 }
 
 let count = 13;
 
-function addlikes(elemento) {
+function addLikes(elemento) {
     count++;
     elemento.innerHTML = count + " Likes";
     alert("Ninja received a like");
@@ -21,18 +21,11 @@ function addlikes(elemento) {
 
 let counter = 37;
 
-function addlikes(elemento) {
+function addLikes(elemento) {
     counter++;
     elemento.innerHTML = counter + " Likes";
     alert("Ninja received a Like");
 }
 
+//this remove palabras reservadas//
 
-
-//this remove palabras reservadas
-//var boton1 = document.querySelector('.btn1');
-
- //   boton1.onclick = function() {
- //       count++;
-//        this.innerHTML = count + " Likes";
-//    }
