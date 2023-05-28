cd2935c9e46f4b8030e2d58da59897fe0a230f21
JavieraValenzuela90 <javiera.valenzuela.p@gmail.com>
2023-05-27 21:04:57 -0400

    Cambios

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+var edad=18;
+var pais="Chile";
+
+if(edad>=21 && pais!="USA") {
+    console.log("Ya eres mayor de edad");
+} else {
+    num=21-edad;
+    console.log("Aun no eres mayor de edad, y te faltan " + num + " años para serlo.");
+}
