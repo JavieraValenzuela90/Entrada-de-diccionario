04d63dc69c08669757ff26d9cf0143f4886d940a
JavieraValenzuela90 <javiera.valenzuela.p@gmail.com>
2023-06-05 10:31:53 -0400

    Act tarea Me Gusta

--- Me Gusta/main2.js
+++ Me Gusta/main2.js
@@ -1,6 +1,6 @@
 function increaseLikes(index) {
-var likeCount = document.querySelectorAll(".likeCount")[index];
-var count = parseInt(likeCount.innerHTML);
-count++;
-likeCount.innerHTML = count;
+    var likeCount = document.querySelectorAll(".likeCount")[index];
+    var count = parseInt(likeCount.innerHTML);
+    count++;
+    likeCount.innerHTML = count;
 }
